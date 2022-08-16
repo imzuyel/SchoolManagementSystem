e7567a0b0f0982840ab9df64103c66319f039fb4
Zuyel Rana <zuyel76@gmail.com>
2022-08-16 20:35:38 +0600

    Delete Unused js

--- resources/views/backend/partials/dataTable.blade.php
+++ resources/views/backend/partials/dataTable.blade.php
@@ -13,19 +13,15 @@
 <script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
 <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/jszip/jszip.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
 <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
+
 <script>
   $(function() {
     $("#example").DataTable({
       "responsive": true,
       "lengthChange": true,
       "autoWidth": false,
-      "buttons": ["pdf", "print"],
       "bDestroy": true,
     }).buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
 
